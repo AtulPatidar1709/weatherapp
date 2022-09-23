7e51fc803a4b9a0eae9476002ed1837941ba354c
AtulPatidar <atulpatidar1709@gmail.com>
2022-09-23 18:30:25 +0530

    changes working new api

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "26c1be505bmsh16a81b6c62b2ed1p112791jsn17bf434b65cf",// enter your rapid api key here
-    "X-RapidAPI-Host": "'wft-geo-db.p.rapidapi.com",
+    "X-RapidAPI-Key": "26c1be505bmsh16a81b6c62b2ed1p112791jsn17bf434b65cf",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/";
-export const WEATHER_API_KEY = "6c29ffe4a8bb27ad20ee8348a82cc82e";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "6c29ffe4a8bb27ad20ee8348a82cc82e"; 
